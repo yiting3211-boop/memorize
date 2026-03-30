1103c0b0d19a8334ebcdc06a638ba4282a3aaa8f
11344153 <yiting.3211@email.com>
2026-03-30 19:00:50 +0800

    feat: add model & viewModel

---
+++ memorize/MemoryGame.swift
@@ -0,0 +1,36 @@
+//
+//  MemoryGame.swift
+//  memorize
+//
+//  Created by 11344153 on 2026/3/30.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    var cards: [Card]
+    
+    init(numberOfPairsOfCards: Int ,
+         createCardContent: (Int) -> CardContent){
+        cards = []
+        //TODO: init cards
+        for index in 0..<numberOfPairsOfCards {
+            cards = []
+            for index in 0..<numberOfPairsOfCards {
+                var cardContent: CardContent = createCardContent(index)
+                cards.append(Card(content:cardContent))
+                cards.append(Card(content:cardContent))
+            }
+        }
+    }
+    func choose(card: Card){
+        
+    }
+    
+    struct Card{
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
