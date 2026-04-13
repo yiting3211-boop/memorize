d6089d1426ac12088761b396dc41e4ba71dee8cc
11344153 <yiting.3211@email.com>
2026-04-13 19:18:48 +0800

    feat: add code

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -10,15 +10,19 @@ import SwiftUI
 struct ContentView: View {
     //var emojis:Array<String> = ["A","A","A","A"]
     //var emojis:[String]= ["A","A","A","A"]
-    var emojis = ["Ａ","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","牛","狗"]
+    //var emojis = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","牛","狗"]
     
-    @State var emojiCount = 6
+    var viewModel:EmojiMemoryGame
     
     var body: some View {
         VStack{
             cardList
+                .animation(.default, value: viewModel.cards)
             Spacer()
-            actionButtons
+            Button("Shuffle"){
+                viewModel.shuffle()
+            }
+            .font(.largeTitle)
         }
         .padding()
         .foregroundStyle(.orange)
@@ -27,47 +31,21 @@ struct ContentView: View {
     var cardList: some View {
         ScrollView{
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 85),spacing: 0)],spacing: 0){
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content:emoji)
+                ForEach(viewModel.cards) { card in
+                    CardView(card:card)
                         .aspectRatio(2/3,contentMode: .fit)
                         .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
                 }
             }
         }
     }
-    var actionButtons: some View {
-        HStack{
-            remove
-            Spacer()
-            add
-        }
-        .font(.largeTitle)
-    }
-    var remove:some View{
-        Button{
-            if emojiCount > 1{
-                emojiCount -= 1
-            }
-        } label:{
-            Image(systemName:"minus.circle")
-        }
-        
-    }
-    var add:some View{
-        Button{
-            if emojiCount < emojis.count{
-                emojiCount += 1
-            }
-        } label:{
-            Image(systemName:"plus.circle")
-        }
-        
-    }
 }
 
 struct CardView: View{
-    @State var isFaceUp: Bool = true
-    var content: String
+    var card:MemoryGame<String>.Card
     var body: some View{
         ZStack{
             let shape = RoundedRectangle(cornerRadius: 20)
@@ -75,20 +53,19 @@ struct CardView: View{
             Group{
                 shape.fill(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content)
+                Text(card.content)
                     .font(Font.system(size:300))
                     .minimumScaleFactor(0.01)
                     .aspectRatio(1,contentMode: .fit)
             }
-            .opacity(isFaceUp ? 1 : 0)
+            .opacity(card.isFaceUp ? 1 : 0)
             
-            shape.opacity(isFaceUp ? 0 : 1)
+            shape.opacity(card.isFaceUp ? 0 : 1)
             
         }
-        .onTapGesture{isFaceUp = !isFaceUp}
-        //.onTapGesture(perform: {isFaceUp = !isFaceUp})
+        .opacity(card.isMatched && !card.isFaceUp ? 0 : 1)
     }
 }
 #Preview {
-    ContentView()
+    ContentView(viewModel:EmojiMemoryGame())
 }
--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent:Equatable {
     
     var cards: [Card]
     
@@ -16,21 +16,56 @@ struct MemoryGame<CardContent> {
         cards = []
         //TODO: init cards
         for index in 0..<numberOfPairsOfCards {
-            cards = []
-            for index in 0..<numberOfPairsOfCards {
-                var cardContent: CardContent = createCardContent(index)
-                cards.append(Card(content:cardContent))
-                cards.append(Card(content:cardContent))
+                let cardContent: CardContent = createCardContent(index)
+            cards.append(Card(content:cardContent, id: "\(index)a"))
+            cards.append(Card(content:cardContent, id: "\(index)b"))
+        }
+        shuffle()
+    }
+    var lastFaceUpIndex: Int?
+    
+    mutating func choose(_ card: Card){
+        if let chosenIndex = index(of: card){
+            if let lastIndex = lastFaceUpIndex{
+                if cards[lastIndex].content == cards[chosenIndex].content{
+                    cards[lastIndex].isMatched = true
+                    cards[chosenIndex].isMatched = true
+                }
+                lastFaceUpIndex = nil
+            }else{
+                for i in 0..<cards.count{
+                    cards[i].isFaceUp = false
+                }
+                lastFaceUpIndex = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp.toggle()
+            }
+        print("cards: \(cards)")
+    }
+    func index(of card: Card) -> Int? {
+        for i in 0..<cards.count{
+            if cards[i].id == card.id {
+                return i
             }
         }
+        return nil
     }
-    func choose(card: Card){
-        
+    mutating func shuffle(){
+        cards.shuffle()
+        print("shuffle cards:\(cards)")
     }
     
-    struct Card{
+    struct Card: Equatable, Identifiable{
+        
+        static func == (lhs:MemoryGame<CardContent>.Card,rhs:
+                       MemoryGame<CardContent>.Card) -> Bool{
+            lhs.content == rhs.content && lhs.isFaceUp ==
+            rhs.isFaceUp && lhs.isMatched == rhs.isMatched && lhs.id == rhs.id
+        }
         var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
+        
+        let id : String
     }
 }
